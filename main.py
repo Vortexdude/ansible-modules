329af536084833e890689c8717ac48192f895ed3
vortexdude <nitinnamdeo456@gmail.com>
2023-11-04 00:04:37 +0530

    separate the filter class for understanding

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from re import split
-import subprocess, os, yaml, json
+import subprocess
+import os
+import yaml
+import json
 
 DEFAULT_FILTER_CONFIG_FILE = 'filters.yml'
 FFMPEG_BIN : list = "ffmpeg -v error -y".split()
@@ -8,16 +10,13 @@ class Utils:
 
     @staticmethod
     def ensure_list(variable):
-        if not isinstance(variable, list):
-            return [variable]
-        return variable
+        return [variable] if not isinstance(variable, list) else variable
 
     @staticmethod
     def json_parsor(file):
         with open(file, 'r') as yaml_in:
             yaml_object = yaml.safe_load(yaml_in)
-            _g = json.dumps(yaml_object)
-            return json.loads(_g)
+            return json.loads(json.dumps(yaml_object))
 
     @staticmethod
     def run_cmd(cmd):
@@ -104,17 +103,50 @@ class Utils:
 
 
 class Filters:
-    pass
 
+    def __init__(self):
+        self.filter_string: list = []
+        self.filter_copy_inputs: list = []
+        self.filter_copy_outputs: list = []
 
-class MediaOperation:
+    def copy(self):
+        filter = ""
+        for i in self.filter_copy_inputs:
+            if "split" not in filter:
+                filter = filter + f"split[{i}]"
+            else:
+                filter = filter + f"[{i}]"
+        self.filter_string.append(filter)
+
+    def scale(self, input, output, resolution: list, *args, **kwargs):
+        filter = ""
+        if resolution:
+            if "scale" not in filter:
+                if len(resolution) <= 1:
+                    filter = filter + f"[{input}]scale=-1:{resolution[0]}"
+                else:    
+                    filter = filter + f"[{input}]scale=width={resolution[0]}:height={resolution[1]}"
+                for args in kwargs.items():
+                    if "gray" in args:
+                        filter = filter + f",format=gray"
+                    if "hflip" in args:
+                        filter = filter + f",hflip"
+        filter = filter + f"[{output}]"
+        self.filter_string.append(filter)
+
+    def overlay(self, inputs:list, x:str, y:str):
+        filter = ""
+        for input in inputs:
+            filter = filter + f"[{input}]"
+        filter = filter + f"overlay=x={x}:y={y}"
+        self.filter_string.append(filter)
+
+class MediaOperation(Filters):
 
     def __init__(self):
         self.file: str = DEFAULT_FILTER_CONFIG_FILE
         self.config: dict = Utils.json_parsor(self.file)
-        self.filter_string: list = []
-        self.filter_copy_inputs: list = []
-        self.filter_copy_outputs: list = []
+        super().__init__()
 
     def generate_cmd(self, filter: str, duration: str = None):
         input_file_path = self.config['files']['input']
@@ -137,14 +169,15 @@ class MediaOperation:
     def filter_generation(self):
         if "filters" in self.config:
             for filter_name, data in self.config['filters'].items():
+
                 if filter_name == 'copy':
                     self.filter_copy_inputs = [data for data in data.keys()]
                     self.filter_copy_outputs = [data for data in data.values()]
-                    self.filter_copy()
+                    super().copy()
 
                 if filter_name == 'scale':
                     for i in data:
-                        self.filter_scale(
+                        super().scale(
                             input=i['input'],
                             output=i['output'],
                             resolution=Utils.ensure_list(i['res']),
@@ -152,44 +185,12 @@ class MediaOperation:
                         )
                 if filter_name =='overlay':
                     for item in data:
-                        self.filter_overlay(
+                        super().overlay(
                             inputs=[item['background'], item['overlay']],
                             x=item['x'],
                             y=item['y']
                         )
             return ";".join(dummy.filter_string)
-
-    def filter_copy(self):
-        filter = ""
-        for i in self.filter_copy_inputs:
-            if "split" not in filter:
-                filter = filter + f"split[{i}]"
-            else:
-                filter = filter + f"[{i}]"
-        self.filter_string.append(filter)
-
-    def filter_scale(self, input, output, resolution: list, *args, **kwargs):
-        filter = ""
-        if resolution:
-            if "scale" not in filter:
-                if len(resolution) <= 1:
-                    filter = filter + f"[{input}]scale=-1:{resolution[0]}"
-                else:    
-                    filter = filter + f"[{input}]scale=width={resolution[0]}:height={resolution[1]}"
-                for args in kwargs.items():
-                    if "gray" in args:
-                        filter = filter + f",format=gray"
-                    if "hflip" in args:
-                        filter = filter + f",hflip"
-        filter = filter + f"[{output}]"
-        self.filter_string.append(filter)
-
-    def filter_overlay(self, inputs:list, x:str, y:str):
-        filter = ""
-        for input in inputs:
-            filter = filter + f"[{input}]"
-        filter = filter + f"overlay=x={x}:y={y}"
-        self.filter_string.append(filter)
 
 dummy = MediaOperation()
 
